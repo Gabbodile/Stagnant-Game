3ea87059eaefdaf9f524173d5b42f237e266e28c
TheRageofGod <spacedwarf1@gmail.com>
2023-06-26 14:45:43 +1000

    Dialogue Array and Annotae
    Put all the Dialogue into an Array for Ease of Access and annotated code

--- Assets/Scripts/Clicker.cs
+++ Assets/Scripts/Clicker.cs
@@ -2,49 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//This Script is designed to use a raycast to figure oput what the player is clicling and if it has a corrisponding action it then calls that action
+//This Script is designed to use a raycast to figure out what the player is clicking and if it has a corrisponding action it then calls that action
 public class Clicker : MonoBehaviour
 {
-    [SerializeField] string obName = null;
+    [SerializeField] string obName = null;//Empty string
 
-    public Events eventScript;
+    public Events eventScript;//Refrences the Event Script
 
     private void Start()
     {
-        eventScript = FindObjectOfType<Events>();
+        eventScript = FindObjectOfType<Events>();//Find the Event scipt in the scene
     }
     void Update()
     {
         if (Input.GetMouseButtonDown(0))//Mouse Input
-        {
-
-            
-
-            RaycastHit2D Hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-            if (Hit.collider !=null)
+        { 
+            RaycastHit2D Hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);//Casts Ray where the mouse is
+            if (Hit.collider !=null)//checks if youve hit something
             {
-                obName = Hit.transform.name;
+                obName = Hit.transform.name;//grabs the hi bjects name and stores it in the empty string
                 Debug.Log(obName);
             }
         }
-        if (obName == "Door")
+        if (obName == "Door")//Checks name
         {
-            eventScript.DoorOpen();
-            obName = "None";
+            eventScript.DoorOpen();//acivates Function in Event script
+            obName = "None";//Resets empty String
         }
-        if (obName == "Toaster")
+        if (obName == "Toaster")//Checks name
         {
-            eventScript.ToasterClicked();
-            obName = "None";   
+            eventScript.ToasterClicked();//acivates Function in Event script
+            obName = "None";//Resets empty String   
         }
-        if (obName == "OldMan")
+        if (obName == "OldMan")//Checks name
         {
-            eventScript.OldManTalk();
-            obName = "None";
+            eventScript.OldManTalk();//acivates Function in Event script
+            obName = "None";//Resets empty String
         }
-        if(obName == "Meds")
+        if(obName == "Meds")//Checks name
         {
-            eventScript.FoundMeds();
+            eventScript.FoundMeds();//acivates Function in Event script
+            obName = "None";//Resets empty String
         }
     }
 }
--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -6,27 +6,37 @@ using UnityEngine.UI;
 using TMPro;
 public class Events : MonoBehaviour
 {
+    //simple int to act as a key
     [SerializeField] int toasterKey = 0;
 
+    //Reffrence to the Bathroom "Scene"
     public GameObject bathroomBackground;
-    public GameObject bathroomDoorBackground;
+    //Array of all other Scenes
     public GameObject[] scenes;
+    //Keeps track of current scene
     public int currentScene = 0;
 
+
+    //Ui Elements
     public TextMeshProUGUI display;
     public Button PrevButton;
     public Button NextButton;
     public Button LeaveButton;
     public float textResetTime = 1f;
 
+    //Dialogue Array
+    public string[] dialogue;
+
+    //Reffrences GameObject
     public GameObject OldMan;
 
+    //Bathroom checks
     bool bathroomOpen = false;
     bool InBathroom = false;
 
     public void Start()
     {
-        scenes[0].SetActive(true);
+        //Sets all but the first "scene" to deactivate
         for (int i = 0; i < scenes.Length; i++)
         {
             scenes[i].SetActive(false);
@@ -36,16 +46,90 @@ public class Events : MonoBehaviour
                 continue;
             }
         }
+
+        //Hides button needed for later
             LeaveButton.gameObject.SetActive(false);
         
     }
     public void Update()
     {
+        //checks if the door has been unlocked
         if (toasterKey >= 3)
         {
             bathroomOpen = true;
         }
-        if (currentScene == scenes.Length - 1)
+
+        ButtonController();//checks where we are in the world and enabes/disables the button when the end is reached
+    }
+    public void SceneChanger()//Lets the Player Leave the Bathroom
+    {
+        InBathroom = false;//says that the player isnt in the bathroom 
+        bathroomBackground.SetActive(false);//disables the bathroom
+        scenes[2].SetActive(true);//enables the bathroom door way
+        LeaveButton.gameObject.SetActive(false);//disables the button used to leave the bathroom
+    }
+
+    public void NextScene()//Summons the next "scene"
+    {
+        scenes[currentScene].SetActive(false);//sets the current scene to false
+        scenes[currentScene + 1].SetActive(true);//sets next scene to true
+        currentScene = currentScene + 1;//updates the current scene
+    }
+    public void PreviousScene()//Summons previous "scene"
+    {
+        scenes[currentScene].SetActive(false);//sets current scene to false
+        scenes[currentScene - 1].SetActive(true);//sets the previous scene to true
+        currentScene = currentScene - 1;//updates the current scene
+    }
+    public void DoorOpen()//opens the door to the bathroom
+    {
+        if (bathroomOpen == true)//checks if the door is unlocked
+        {
+            InBathroom = true;//says we are in the bathroom
+            bathroomBackground.SetActive(true);//Sets the bathroom to active
+            scenes[currentScene].SetActive(false);//sets the hallway to inactive
+            LeaveButton.gameObject.SetActive(true);//turns on the button used to lead
+            NextButton.interactable = false;//Disables Button
+           PrevButton.interactable = false;//Disables Button
+        }
+        else//If door is locked
+        {
+            display.text = dialogue[0];//grabs Dialogue from array
+            StartCoroutine(resetText());//Resets the Text box
+        }
+    }
+
+    public void ToasterClicked()//used when the toaster is clicked
+    {
+        toasterKey = toasterKey + 1;//increases toaster key
+        display.text = dialogue[1];//grabs Dialogue from array
+        StartCoroutine(resetText());//Resets the Text box
+    }
+
+    public void OldManTalk()//used when the old man is clicked
+    {
+        display.text = dialogue[2];//grabs Dialogue from array
+        textResetTime = 5f;//Changes the time it takes for text to disapear
+        StartCoroutine(resetText());//Resets the Text box
+        textResetTime = 1f;//Changes the time it takes for text to disapear
+    }
+
+    public void FoundMeds()
+    {
+        display.text = dialogue[3];//grabs Dialogue from array
+        OldMan.SetActive(false);//removes the old man
+    }
+
+    public IEnumerator resetText()//resets the text box
+    {
+        yield return new WaitForSeconds(textResetTime);//Delays the Reset
+        display.text = "";//Replaces previous Text with blank
+        StopCoroutine(resetText());//Stops reseting 
+    }
+
+    public void ButtonController()// controlls the buttons based on the length of the scene Array
+    {
+        if (currentScene == scenes.Length - 1)//if at the end of the Array
         {
             NextButton.interactable = false;
         }
@@ -53,84 +137,14 @@ public class Events : MonoBehaviour
         {
             NextButton.interactable = true;
         }
-        if (currentScene == 0)
+        if (currentScene == 0)//if at the start if the Array
         {
             PrevButton.interactable = false;
         }
-        else if(InBathroom == false)
+        else if (InBathroom == false)//if not in the bathroom
         {
             PrevButton.interactable = true;
         }
-    }
-    public void SceneChanger()
-    {
-        InBathroom = false;
-        bathroomBackground.SetActive(false);
-        bathroomDoorBackground.SetActive(true);
-        LeaveButton.gameObject.SetActive(false);
-        NextButton.interactable = false;
-        PrevButton.interactable = true;
-        
-    }
-
-    public void NextScene()
-    {
-        
-        scenes[currentScene].SetActive(false);
-        scenes[currentScene + 1].SetActive(true);
-        currentScene = currentScene + 1;
-    }
-    public void PreviousScene()
-    {
-        
-        scenes[currentScene].SetActive(false);
-        scenes[currentScene - 1].SetActive(true);
-        currentScene = currentScene - 1;
-    }
-    public void DoorOpen()
-    {
-        if (bathroomOpen == true)
-        {
-            InBathroom = true;
-            bathroomBackground.SetActive(true);
-            bathroomDoorBackground.SetActive(false);
-            LeaveButton.gameObject.SetActive(true);
-            NextButton.interactable = false;
-            PrevButton.interactable = false;
-        }
-        else
-        {
-            display.text = "The Door Is Locked";
-            StartCoroutine(resetText());
-        }
-    }
-
-    public void ToasterClicked()
-    {
-        toasterKey = toasterKey + 1;
-        display.text = "*Rattle*";
-        StartCoroutine(resetText());
-    }
-
-    public void OldManTalk()
-    {
-        display.text = "That Damn Toaster is Messing with the Locks Again, can you try giving it a couple of Wacks for me? I need my Medicine from the Bathroom";
-        textResetTime = 5f;
-        StartCoroutine(resetText());
-        textResetTime = 1f;
-    }
-
-    public void FoundMeds()
-    {
-        display.text = "congrats You found Your Schizophrenia Meds now would you kindly take them";
-        OldMan.SetActive(false);
-    }
-
-    public IEnumerator resetText()
-    {
-        yield return new WaitForSeconds(textResetTime);
-        display.text = "";
-        StopCoroutine(resetText());
     }
 
 }
