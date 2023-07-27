ac31120e58e03fb4dcc7492584c6f5fb207418e2
TheRageofGod <spacedwarf1@gmail.com>
2023-07-27 12:23:44 +1000

    Dialogue Work

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class NPC : MonoBehaviour
 {
     public Sprite Portait;
-    public string dialogueText;
+    public string[] dialogueText;
+    public int dialogueSelect;
     public UI uiManager;
+    public CheckList CL;
     public int energyValue = 1;
+    public string[] EventCheck;
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Hit That NPC");
     }
     public void Dialogue()
     {
+        for (int i = 0; i < CL.structArray.Length; i++) 
+        {
+            for (int j = 0; j < EventCheck.Length; j++)
+            {
+                if (CL.structArray[i].Event == EventCheck[j] && CL.structArray[i].Check == true)
+                {
+                    dialogueSelect = dialogueSelect + 1;
+                }
+            }
+        }
         uiManager.DialogueActivate();
-        uiManager.DialogueBox.text = dialogueText;
+        uiManager.DialogueBox.text = dialogueText[dialogueSelect];
         uiManager.PortraitDisplay.sprite = Portait;
         Debug.Log("Start Dialogue");
         //energytracker.energy = energytraker.energy - energyValue
