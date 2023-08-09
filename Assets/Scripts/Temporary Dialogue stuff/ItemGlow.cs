9fa775261e039228118f18f7be62a08086eb9edb
Gabbodile <94325069+Gabbodile@users.noreply.github.com>
2023-08-10 09:17:57 +1000

    fixed the interactions

--- Assets/Scripts/Temporary Dialogue stuff/ItemGlow.cs
+++ Assets/Scripts/Temporary Dialogue stuff/ItemGlow.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ItemGlow : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Renderer itemColour;
+    public GameObject dialogueBox;
+    public TMP_Text dialogueText;
+    public TMP_Text nameText;
+    public string textbox = "";
+    public string namebox = "";
+
     void Start()
     {
-        
+        itemColour = gameObject.GetComponent<Renderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseOver()
     {
-        
+        itemColour.material.color = Color.yellow;
+    }
+
+    private void OnMouseExit()
+    {
+        itemColour.material.color = Color.white;
+    }
+
+    private void OnMouseDown()
+    {
+        dialogueBox.SetActive(true);
+        nameText.text = namebox;
+        dialogueText.text = textbox;
     }
 }
